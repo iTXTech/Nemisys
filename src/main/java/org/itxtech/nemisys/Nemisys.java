a9edaf6827bf766c999d9ae775c33191c39072df
Jarne <jarnejost@icloud.com>
2016-09-23 01:15:11 +0200

    Update to 0.15.9 (#52)
    * Update to 0.15.9
    
    * New protocol version

--- src/main/java/org/itxtech/nemisys/Nemisys.java
+++ src/main/java/org/itxtech/nemisys/Nemisys.java
@@ -9,8 +9,8 @@ public class Nemisys {
     public final static String VERSION = "1.0dev";
     public final static String API_VERSION = "1.0.0";
     public final static String CODENAME = "Aegis";
-    public final static String MINECRAFT_VERSION = "v0.15.6 alpha";
-    public final static String MINECRAFT_VERSION_NETWORK = "0.15.6";
+    public final static String MINECRAFT_VERSION = "v0.15.9 alpha";
+    public final static String MINECRAFT_VERSION_NETWORK = "0.15.9";
 
     public final static String PATH = System.getProperty("user.dir") + "/";
     public final static String DATA_PATH = System.getProperty("user.dir") + "/";
