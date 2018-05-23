5aa0e2a837bae801df4679f119defec08e8e7555
Mr. Sheep <mistersheepbusiness@gmail.com>
2018-05-23 13:07:00 -0400

    Bump Protocol to 1.4.0

--- src/main/java/org/itxtech/nemisys/network/protocol/mcpe/ProtocolInfo.java
+++ src/main/java/org/itxtech/nemisys/network/protocol/mcpe/ProtocolInfo.java
@@ -9,9 +9,9 @@ public interface ProtocolInfo {
     /**
      * Actual Minecraft: PE protocol version
      */
-    int CURRENT_PROTOCOL = 137;
-    String MINECRAFT_VERSION = "v1.2.1";
-    String MINECRAFT_VERSION_NETWORK = "1.2.1";
+    int CURRENT_PROTOCOL = 261;
+    String MINECRAFT_VERSION = "v1.4.0";
+    String MINECRAFT_VERSION_NETWORK = "1.4.0";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
